4d7a4340358b0de961405db0d2017c6e7b26f0c3
Jenny Digné <jenny_digne@hotmail.com>
2025-09-04 16:53:08 +0200

    gjort async funktion i route

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -2,10 +2,8 @@ import { mockSearchResponse } from "@/app/types/mockData";
 import { SearchCriteriaSchema } from "@/app/types/shared";
 import type { SearchResponse } from "@/app/types/shared";
 
-//exempel på struktur
 export async function POST(req: Request) {
-  const body = await req.json();
-  // validera body
-  // anropa AI eller dataset
-  // returnera typad JSON
+    const body = SearchCriteriaSchema.parse(await req.json());
+    const response: SearchResponse = mockSearchResponse;
+    return new Response(JSON.stringify(response), { status: 200 });
 }
