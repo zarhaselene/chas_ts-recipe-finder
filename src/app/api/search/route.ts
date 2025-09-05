53fe80c8556893ef5fc738053fb1f040e0e80776
Jenny Digné <jenny_digne@hotmail.com>
2025-09-05 14:19:33 +0200

    lagt till filter för ingredienser

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -5,7 +5,13 @@ import type { SearchResponse } from "@/app/types/shared";
 export async function POST(req: Request) {
     try {
         const body = SearchCriteriaSchema.parse(await req.json());
-        const response: SearchResponse = mockSearchResponse;
+        const filteredResult = mockSearchResponse.results.filter(recipe => {
+            return body.ingredients.every(ing => recipe.ingredients.includes(ing));
+        });
+        const response: SearchResponse = {
+            ...mockSearchResponse,
+            results: filteredResult
+        }
         return new Response(JSON.stringify(response), {
             status: 200,
             headers: { "Content-Type": "application/json" },
@@ -14,10 +20,7 @@ export async function POST(req: Request) {
         console.error("API error:", error);
         return new Response(
             JSON.stringify({ error: "Invalid request or server error" }),
-            {
-                status: 400,
-                headers: { "Content-Type": "application/json" },
-            }
+            { status: 400, headers: { "Content-Type": "application/json" } }
         );
     }
 }
